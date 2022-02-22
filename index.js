6283a0173289ca84d78c9cf81f55c031dc74db56
AnnaKuci <ak.kucirkova@gmail.com>
2022-02-22 17:30:32 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-//import './style.css';
+//import './style.css'; jednoradkovy koment
+/*jojoasd. vice radkovy koment v kodu
+flakshdk
+*/
 console.log("Ahoj svete")
 import './ukol1'
-
+alert("pepa");
+prompt("kolik je ti let?");
