439206b188a881fb44fe51c65f5aa785c6eb8be5
AnnaKuci <ak.kucirkova@gmail.com>
2022-02-22 17:51:13 +0100

    2 files modified

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 /*jojoasd. vice radkovy koment v kodu
 flakshdk
 */
-console.log("Ahoj svete")
-import './ukol1'
-alert("pepa");
-prompt("kolik je ti let?");
+console.log("Ahoj svete");
+import './ukol1';
+//alert("pepa");
+//prompt("kolik je ti let?");
+console.log(jmeno);
+let jmeno="PETR";
+let vek = true;
--- ukol1.js
+++ ukol1.js
@@ -1,1 +1,8 @@
-console.log("pozdrav z ukolu1")
+//console.log("pozdrav z ukolu1")
+let a = 5 ;
+let b = 15 ;
+
+
+let soucet = a + b + 100;
+
+console. log(soucet);
