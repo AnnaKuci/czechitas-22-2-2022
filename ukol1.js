5fb3c71341b3c0df20745f0c23b011642019a240
AnnaKuci <ak.kucirkova@gmail.com>
2022-02-22 17:59:25 +0100

    1 file modified

--- ukol1.js
+++ ukol1.js
@@ -4,5 +4,20 @@ let b = 15 ;
 
 
 let soucet = a + b + 100;
+let jmeno = "anna";
+let prijmeni = " kuci";
 
-console. log(a ** b);
+console.log(jmeno +  prijmeni);
+console.log(a ** b); 
+console.log(5 % 2); //zbytek 1 pri 5/2
+
+let usernameOfCurrentUser = "cammel case"
+
+
+let minutVHodine = 60;
+let hodinVeDne = 24;
+let dnuVLednu = 31;
+
+let vysledek = minutVHodine * hodinVeDne * dnuVLednu;
+
+console.log("V lednu je celkem" vysledek "dnu")
